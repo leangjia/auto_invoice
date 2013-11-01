3d507273caa602ea44eedfbfee9beea2e665bbdf
baggio <baggio@baggio-ThinkPad-E420.(none)>
2013-11-01 10:03:35 +0800

    :auto_invoice

---
+++ auto_invoice/__openerp__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Auto Invioce',
+    'version': '1.0',
+    'category': 'sale',
+    'description': u"""
+    Auto invioce
+    """,
+    'author': 'ShineIT<contact@openerp.cn>',
+    'depends': ['sale_stock',],
+    'data': [
+        'sale_workflow.xml',
+    ],
+    'demo': [],
+    'installable': True,
+    'auto_install': False,
+}
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
