ff1c8c41f511a64e058446205f2ac85c25ffd59b
baggio <baggio@baggio-ThinkPad-E420.(none)>
2013-11-01 17:36:16 +0800

    :

--- auto_invoice/sale_stock.py
+++ auto_invoice/sale_stock.py
@@ -14,7 +14,7 @@ class sale_order(osv.osv):
                   ('prepaid', 'Before Delivery'),
                   ('autoinv', 'Auto Invoice'),
             ], 'Create Invoice', required=True, readonly=True, states={'draft': [('readonly', False)], 'sent': [('readonly', False)]},
-            help="""On demand: A draft invoice can be created from the sales order when needed. \nOn delivery order: A draft invoice can be created from the delivery order when the products have been delivered. \nBefore delivery: A draft invoice is created from the sales order and must be paid before the products can be delivered."""),    
+            help="""Auto Invoice: A draft invoice can be created from the sales order when you confirm the order. \nOn demand: A draft invoice can be created from the sales order when needed. \nOn delivery order: A draft invoice can be created from the delivery order when the products have been delivered. \nBefore delivery: A draft invoice is created from the sales order and must be paid before the products can be delivered."""),    
     }
 
     _defaults = {
